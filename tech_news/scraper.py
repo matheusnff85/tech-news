cc854b5b07ae2f197d89301809386fdc2c312f79
matheusnff85 <math.marinho154@gmail.com>
2023-01-17 15:51:55 -0300

    feat: requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -19,7 +20,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news_links = selector.css(".entry-title h2 a::attr(href)").getall()
+    return news_links
 
 
 # Requisito 3
