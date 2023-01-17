74ad43fa8fc14b01d77134b642d7ee9c86ac985b
matheusnff85 <math.marinho154@gmail.com>
2023-01-17 16:05:06 -0300

    feat; implementa função scrape

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,9 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page_link = selector.css(".next::attr(href)").get()
+    return next_page_link
 
 
 # Requisito 4
