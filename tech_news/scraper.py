e22ae6aa017c4ef5116b6a1e2f56850b988c32fa
matheusnff85 <math.marinho154@gmail.com>
2023-01-17 17:49:19 -0300

    feat:  implementa requisito 4 e funcao para limpar html do texto

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+import re
 
 
 # Requisito 1
@@ -33,8 +34,43 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
+def remove_html(text):
+    pattern = re.compile("<.*?>")
+    return re.sub(pattern, "", text)
+
+
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    news_infos = {}
+    selector = Selector(text=html_content)
+
+    news_infos["url"] = (
+        selector.css("link[rel='canonical']::attr(href)").get()
+    )
+    news_infos["title"] = (
+        selector.css(".entry-title::text").get().strip()
+    )
+    news_infos["timestamp"] = (
+        selector.css(".meta-date::text").get()
+    )
+    news_infos["writer"] = (
+        selector.css(".author a::text").get()
+    )
+    news_infos["comments_count"] = (
+        len(selector.css("comment-list li").getall()) or 0
+    )
+    news_infos["summary"] = (
+        remove_html(selector.css(".entry-content p").get()).strip()
+    )
+    news_infos["tags"] = (
+        selector.css(".post-tags li a::text").getall()
+    )
+    news_infos["category"] = (
+        selector.css(".meta-category .label::text").get()
+    )
+
+    if not news_infos["title"] or not news_infos["summary"]:
+        return None
+    return news_infos
 
 
 # Requisito 5
