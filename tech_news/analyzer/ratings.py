68ad3c906bbbd2b9a92b5a20b4be1c8defa36e63
matheusnff85 <math.marinho154@gmail.com>
2023-01-18 14:14:57 -0300

    feat: implementa search top 5 news req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+from tech_news.database import find_news
+from operator import itemgetter
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_comments = []
+    for new in find_news():
+        news_comments.append(new)
+    sort_title = sorted(news_comments, key=itemgetter("title"))
+    sort_comments = sorted(
+        sort_title, key=itemgetter("comments_count"), reverse=True
+    )
+    return [(new["title"], new["url"]) for new in sort_comments][:5]
 
 
 # Requisito 11
