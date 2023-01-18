b6696ed914d64abc0119ffa93bb7a0c7403aafac
matheusnff85 <math.marinho154@gmail.com>
2023-01-18 14:38:11 -0300

    feat: implementa menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,17 @@
+options_array = [
+    'Selecione uma das opções a seguir:\n',
+    ' 0 - Popular o banco com notícias;\n',
+    ' 1 - Buscar notícias por título;\n',
+    ' 2 - Buscar notícias por data;\n',
+    ' 3 - Buscar notícias por tag;\n',
+    ' 4 - Buscar notícias por categoria;\n',
+    ' 5 - Listar top 5 notícias;\n',
+    ' 6 - Listar top 5 categorias;\n',
+    ' 7 - Sair.\n',
+]
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    option = input("".join(options_array))
+    return option
